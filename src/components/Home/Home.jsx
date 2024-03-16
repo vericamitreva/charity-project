0b735ff88e0759af9d9895d069aa852d01c8ecab
MarijaBute <mbuteska@gmail.com>
2024-03-16 17:28:52 +0100

    Home about us section added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,14 @@
 import "./home.css"
 import HeroSection from "./components/HeroSection/HeroSection"
 import WidgetSection from "./components/WidgetSection/WidgetSection"
+import AboutSection from "./components/AboutSection/AboutSection"
 
 export default function Home() {
     return (
         <div className="home-container">
             <HeroSection/>
             <WidgetSection/>
+            <AboutSection />
         </div>
     )
 }
