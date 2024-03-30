5dcbfc8a1171ff3527805c06b1bf95e9c45af18e
MarijaBute <mbuteska@gmail.com>
2024-03-30 15:58:58 +0100

    Testimonial and volunteer sections added

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -4,6 +4,8 @@ import WidgetSection from "./components/WidgetSection/WidgetSection"
 import AboutSection from "./components/AboutSection/AboutSection"
 import FeaturedCampaigns from "./components/CampaignsSection/CampaignsSection"
 import TeamSection from "./components/TeamSection/TeamSection"
+import TestimonialSection from "./components/TestimonialSection/TestimonialSection"
+import Volunteer from "./components/VolunteerSection/VolunteerSection"
 
 export default function Home() {
     return (
@@ -13,6 +15,8 @@ export default function Home() {
             <AboutSection />
             <FeaturedCampaigns />
             <TeamSection />
+            <TestimonialSection />
+            <Volunteer />
         </div>
     )
 }
